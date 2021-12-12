a30e07ba11cb0175f0b8c12e3c6d667e4523fe34
watano1168 <watano1168@gmail.com>
2021-12-12 17:30:55 +0900

    tmp commit

--- src/command-parser/parser.ts
+++ src/command-parser/parser.ts
@@ -1,5 +1,5 @@
 import { interpretCommand, InterpretFormat, InterpretResult } from "./interpreter";
-import { convertParameter, ConvertType } from "./converter";
+import {convertParameter, ConvertType, SupportTypes} from "./converter";
 import {
   CommandFormatArguments,
   CommandFormatOptions,
@@ -10,10 +10,24 @@ export type ConvertTypeSet<TConvertPatternSet extends ConvertPatternSet> = {
   [key in keyof TConvertPatternSet]: ConvertType<TConvertPatternSet[key]>;
 };
 
+export type ConvertTypeSetBase = {
+  [key: string]: ConvertType<SupportTypes>;
+};
+
+export type ConvertPatternSetOf<TConvertTypeSet extends ConvertTypeSetBase> = {
+  [key in keyof TConvertTypeSet]: Exclude<ReturnType<TConvertTypeSet[key]["converter"]>, undefined>
+};
+
 export type CommandFormatDefault<TConvertPatternSet extends ConvertPatternSet> = CommandFormat<
   TConvertPatternSet,
   CommandFormatArguments<TConvertPatternSet>,
   CommandFormatOptions<TConvertPatternSet>
+>;
+
+export type CommandFormatOn<TConvertTypeSet extends ConvertTypeSetBase> = CommandFormat<
+  ConvertPatternSetOf<TConvertTypeSet>,
+  CommandFormatArguments<ConvertPatternSetOf<TConvertTypeSet>>,
+  CommandFormatOptions<ConvertPatternSetOf<TConvertTypeSet>>
 >;
 
 export type CommandFormat<
@@ -35,6 +49,11 @@ export type CommandResult<
   readonly arguments: CommandResultArguments<TConvertPatternSet, TArguments>;
   readonly options: CommandResultOptions<TConvertPatternSet, TOptions>;
 };
+
+export type CommandResultOf<
+  TConvertTypeSet extends ConvertTypeSetBase,
+  TFormat extends CommandFormat<ConvertPatternSetOf<TConvertTypeSet>, CommandFormatArguments<ConvertPatternSetOf<TConvertTypeSet>>, CommandFormatOptions<ConvertPatternSetOf<TConvertTypeSet>>>,
+> = CommandResult<ConvertPatternSetOf<TConvertTypeSet>, TFormat["arguments"], TFormat["options"]>;
 
 type Parameter<TConvertPatternSet extends ConvertPatternSet = ConvertPatternSet> = {
   readonly name: string;
@@ -78,6 +97,8 @@ type CommandResultOptions<
 > = {
   [key in keyof TOptions]?: TConvertPatternSet[TOptions[key]["type"]];
 };
+
+/*
 
 function convertArguments<
   TConvertPatternSet extends ConvertPatternSet,
@@ -139,7 +160,7 @@ export function parseCommand<
 ): CommandResult<TConvertPatternSet, TArguments, TOptions> | undefined {
   const interpretFormat: InterpretFormat<TConvertPatternSet, TArguments, TOptions> = {
     prefixes: format.prefixes,
-    argumentsCount: format.arguments, // It's needed to make sure that value o equals "format.arguments.length".
+    argumentsCount: format.arguments,
     optionsName: format.options
   } as const;
   const interpretation = interpretCommand<TConvertPatternSet, TArguments, TOptions>(command, interpretFormat);
@@ -153,5 +174,114 @@ export function parseCommand<
       types
     ),
     options: convertOptions<TConvertPatternSet, TArguments, TOptions>(interpretation, format, types)
+  };
+}
+
+export function parseCommandInfer<
+  TConvertTypeSet extends ConvertTypeSetBase,
+  TFormat extends CommandFormat<
+    ConvertPatternSetOf<TConvertTypeSet>,
+    CommandFormatArguments<ConvertPatternSetOf<TConvertTypeSet>>,
+    CommandFormatOptions<ConvertPatternSetOf<TConvertTypeSet>>
+  >
+>(
+  command: string,
+  format: TFormat,
+  types: TConvertTypeSet
+): CommandResultOf<TConvertTypeSet, TFormat> | undefined {
+  return parseCommand<
+    ConvertPatternSetOf<TConvertTypeSet>,
+    TFormat["arguments"],
+    TFormat["options"]
+  >(command, format, types);
+}
+*/
+
+function convertArguments<
+  TConvertTypeSet extends ConvertTypeSetBase,
+  TArguments extends CommandFormatArguments<ConvertPatternSetOf<TConvertTypeSet>>,
+  TOptions extends CommandFormatOptions<ConvertPatternSetOf<TConvertTypeSet>>
+  >(
+  values: InterpretResult<ConvertPatternSetOf<TConvertTypeSet>, TArguments, TOptions>,
+  format: CommandFormat<ConvertPatternSetOf<TConvertTypeSet>, TArguments, TOptions>,
+  types: TConvertTypeSet
+): CommandResultArguments<ConvertPatternSetOf<TConvertTypeSet>, TArguments> {
+  const parameters = values.arguments.map((argument, index) => {
+    const fmt = format.arguments[index];
+    if (!fmt) throw Error();
+    const param = {
+      name: `${(index + 1).toString()} 番目の引数`,
+      description: fmt.description,
+      value: values.arguments[index],
+      convertType: types[fmt.type]
+    };
+    return convertParameter(param);
+  });
+  return parameters as CommandResultArguments<ConvertPatternSetOf<TConvertTypeSet>, TArguments>;
+}
+
+function convertOptions<
+  TConvertTypeSet extends ConvertTypeSetBase,
+  TArguments extends CommandFormatArguments<ConvertPatternSetOf<TConvertTypeSet>>,
+  TOptions extends CommandFormatOptions<ConvertPatternSetOf<TConvertTypeSet>>
+  >(
+  values: InterpretResult<ConvertPatternSetOf<TConvertTypeSet>, TArguments, TOptions>,
+  format: CommandFormat<ConvertPatternSetOf<TConvertTypeSet>, TArguments, TOptions>,
+  types: TConvertTypeSet
+): CommandResultOptions<ConvertPatternSetOf<TConvertTypeSet>, TOptions> {
+  const init: CommandResultOptions<ConvertPatternSetOf<TConvertTypeSet>, TOptions> = {};
+  return Object.keys(format.options).reduce((prev, curr) => {
+    const key = curr as keyof TOptions;
+    const value: string | undefined = values.options[key];
+    if (!value) return prev;
+    const param = {
+      name: `オプション ${curr}`,
+      description: format.options[key].description,
+      value: value,
+      convertType: types[format.options[key].type]
+    };
+
+    prev[key] = convertParameter(param);
+    return prev;
+  }, init);
+}
+
+export function parseCommand<
+  TConvertTypeSet extends ConvertTypeSetBase,
+  TFormat extends CommandFormat<
+    ConvertPatternSetOf<TConvertTypeSet>,
+    CommandFormatArguments<ConvertPatternSetOf<TConvertTypeSet>>,
+    CommandFormatOptions<ConvertPatternSetOf<TConvertTypeSet>>
+    >
+  >(
+  command: string,
+  format: TFormat,
+  types: TConvertTypeSet
+): CommandResultOf<TConvertTypeSet, TFormat> | undefined {
+  const interpretFormat: InterpretFormat<
+    ConvertPatternSetOf<TConvertTypeSet>,
+    TFormat["arguments"],
+    TFormat["options"]
+  > = {
+    prefixes: format.prefixes,
+    argumentsCount: format.arguments,
+    optionsName: format.options
+  } as const;
+
+  const interpretation = interpretCommand<
+    ConvertPatternSetOf<TConvertTypeSet>,
+    TFormat["arguments"],
+    TFormat["options"]
+  >(command, interpretFormat);
+  if (!interpretation) return;
+
+  return {
+    prefix: interpretation.prefix,
+    arguments: convertArguments<TConvertTypeSet, TFormat["arguments"], TFormat["options"]>(
+      interpretation,
+      format,
+      types
+    ),
+    options: convertOptions<TConvertTypeSet, TFormat["arguments"], TFormat["options"]>(interpretation, format, types)
   };
 }
---
+++ src/general.ts
@@ -0,0 +1,92 @@
+import { parseCommand } from "./command-parser/parser";
+
+const types = {
+  integer: {
+    name: "整数",
+    converter: (value: string) => {
+      const num = parseInt(value);
+      if (isNaN(num)) return;
+      return num;
+    }
+  } as const,
+  float: {
+    name: "実数",
+    converter: (value: string) => {
+      const num = parseFloat(value);
+      if (isNaN(num)) return;
+      return num;
+    }
+  } as const,
+  string: {
+    name: "文字列",
+    converter: (value: string) => {
+      return value;
+    }
+  } as const
+} as const;
+
+function parse(command: string) {
+  return parseCommand(command, format, types);
+}
+
+const format = {
+  prefixes: ["/search-message", "/message search"] as const,
+  arguments: [
+    {
+      name: "テスト文字列（引数1）",
+      description: "文字列を入力してね（必須）",
+      type: "string"
+    } as const,
+    {
+      name: "テスト実数（引数2）",
+      description: "実数を入力してね（必須）",
+      type: "float"
+    } as const,
+    {
+      name: "テスト整数（引数3）",
+      description: "整数を入力してね（必須）",
+      type: "integer"
+    } as const
+  ] as const,
+  options: {
+    content: {
+      name: "テスト文字列（オプション）",
+      description: "文字列を入力してね（任意）",
+      type: "string"
+    } as const,
+    page: {
+      name: "テスト整数（オプション）",
+      description: "整数を入力してね（任意）",
+      type: "integer"
+    } as const
+  } as const
+} as const;
+
+const commands = [
+  "/search-message aaaaa 11.1 100 --content alpha --page 3",
+  "/message search bbbbb 22.2 200",
+  "/search-message ccccc 33.3 300 --content gamma",
+  "/create-message ddddd 44.4 400 --content gamma",
+  "/search-message ddddd 55.5 3.14159 --content gamma",
+  "/search-message eeeee 66.6 600 1209 --content gamma",
+  "/search-message fffff 77.7 --content gamma",
+  "/search-message ggggg 88.8 800 --content gamma --animal dog",
+  "/search-message iiiii mmmmm 900 --content gamma",
+  "/search-message 111 99.9 1000 --page hoge"
+];
+
+for (const command of commands) {
+  console.log(command);
+  try {
+    const values = parse(command);
+    const r = values?.arguments;
+    console.log(values);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.log("unknown error");
+    }
+  }
+  console.log();
+}
